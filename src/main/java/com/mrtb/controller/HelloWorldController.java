9a74a4c2f469ada3c2448ba3750ac6b1db890de8
mrtb_2000 <mrtbeheshti@gmail.com>
2022-08-28 10:33:06 +0430

    second task finished. the route response returns a json cotaining message and time

--- src/main/java/com/mrtb/controller/HelloWorldController.java
+++ src/main/java/com/mrtb/controller/HelloWorldController.java
@@ -3,13 +3,21 @@ package com.mrtb.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.JSONObject;
+
+import java.sql.Timestamp;
 
 @RestController
 public class HelloWorldController {
     @RequestMapping("/")
-    public String hello(
+    public JSONObject hello(
             @RequestParam(required = false,defaultValue = "World")
                 String name){
-        return "Hello  "+name.substring(0, 1).toUpperCase() + name.substring(1)+"!";
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        String message = "Hello  "+name.substring(0, 1).toUpperCase() + name.substring(1)+"!";
+        JSONObject obj= new JSONObject();
+        obj.put("message",message);
+        obj.put("time", timestamp);
+        return obj;
     }
 }
