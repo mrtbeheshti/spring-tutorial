7d267a8a7e1a9a0102d011b39369fdf292710413
mrtb_2000 <mrtbeheshti@gmail.com>
2022-08-28 09:59:54 +0430

    now route get name from a query param, default is world

--- src/main/java/com/mrtb/controller/HelloWorldController.java
+++ src/main/java/com/mrtb/controller/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.mrtb.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     @RequestMapping("/")
-    public String hello(){
-        return "Hello World";
+    public String hello(
+            @RequestParam(required = false,defaultValue = "World")
+                String name){
+        return "Hello  "+name.substring(0, 1).toUpperCase() + name.substring(1)+"!";
     }
 }
