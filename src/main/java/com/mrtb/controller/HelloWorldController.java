d6d0457d381f22988f56138d350df0030c8bbf8e
mrtb_2000 <mrtbeheshti@gmail.com>
2022-08-31 16:47:20 +0430

    latest changes for accept language, internal server error.

--- src/main/java/com/mrtb/controller/HelloWorldController.java
+++ src/main/java/com/mrtb/controller/HelloWorldController.java
@@ -1,19 +1,25 @@
 package com.mrtb.controller;
 
-import com.mrtb.Enities.User;
-import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.Timestamp;
 import java.util.Locale;
 
-@Slf4j
+
 @Controller
 public class HelloWorldController {
+
+    @Autowired
+    MessageSource messageSource;
+
     @RequestMapping("/")
-    public User getUser(
+    public JSONObject getUser(
             @RequestHeader (
                     name= "Accept-Language",required = false
             )final Locale locale,
@@ -21,7 +27,13 @@ public class HelloWorldController {
                     required = false,defaultValue = "World"
             )final String name
             ) {
-        User user = new User(name);
-        return user;
+        JSONObject response = new JSONObject();
+        String message = messageSource.getMessage("message.hello", null, locale)+" "+name;
+//        String message = "Hello " + name;
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        response.put("message",message);
+        response.put("time", timestamp);
+        return response;
     }
 }
